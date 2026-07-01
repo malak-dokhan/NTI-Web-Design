e56825ef9c3c969bae9e1f765ee5025119029e55
Malak Dokhan <malakdokhan21@gmail.com>
2026-07-01 14:24:25 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+//function looping (startNum,endNum,breakNum,contNum)
+// break=stop , cont=skip
+//looping(1,10,8,3)
+//looping(0,10)
+
+// function looping (startNum =0 ,endNum =20,breakNum,contNum){
+// for (var i = startNum; i <=endNum; i++) {
+//         if (i === breakNum) {
+//             break;
+//         }
+//         if (i === contNum) {
+//             continue;
+//         }
+//         console.log(i);
+//     }
+// }
+// looping(0,15)
+// looping(1,10,8,3)
+// var courses =[ "html" , "css" , "js" , "ts" , "react"];
+// var userCourse = prompt("Enter course name" );
+// var found = false;
+// for (var i = 0; i < courses.length; i++) {
+//     if (courses[i].toLowerCase() === userCourse.toLowerCase()) {
+//         console.log(courses[i]);
+//         found = true;
+//         break;
+//     }
+// }
+// if (found === false) {
+//     console.log("Not Found");
+//     courses.push("userCourse");
+//     console.log("userCourse");
+// }
+// var city = "cairo";
+// console.log("city");
+// var age = 16;
+// if(age>=18) {
+//     console.log("Adult");
+// }
+// console.log("welcome");
+// var num = 7;
+// if( num==7){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+// console.log("done");
+// var degree = prompt("Enter your degree");
+// if(degree>=50){
+//     console.log("pass");
+// } else{
+//     console.log("fail");
+// }
+// var user =prompt("How old are you?");
+// if(user>=18){
+// console.log("Welcome");
+// }else{
+//     console.log("Sorry")
+// }
+// var student =prompt("What is number?");
+// if(student>0){
+//     console.log("positive");
+// }else{
+//     console.log("negative");
+// }
+// NUM TASK
+// var number =prompt("What is number?");
+// if(number>=100){
+//     console.log("Big");
+// }else if(number<=50){
+//     console.log("small");
+// }else if(number>=50 && number<=100){
+//     console.log("Medium");
+// }
+// USERNAME AND PASSWORD TASK
+// var userName =prompt("Enter username please.");
+// var password =prompt("Enter password please.");
+// if(userName=="malak" && password==="1234"){
+//     console.log("login success");
+// }else{
+//     console.log("wrong username or password");
+// }
+// ATM TASK
+// var balance =prompt("what is your balance?");
+// if(balance>=5000){
+//     console.log("vip");
+// }else if(balance>=1000){
+//     console.log("normal");
+// }else(balance<1000){
+//     console.log("low balance");
+// }
+// if(balance>=100){
+//     console.log("you can buy");
+// }else{
+//     console.log("not enough balance");
+// }
+//loop task
+// for(var i=2; i<=10; i++){
+//     if(i==3 ||i==5 ||i==7 ||i==9);
+//     continue;
+//     console.log(i);
+// }
+//continue task
+// for(var i=2; i<=10; i++){
+//     if(i==5);
+//     continue;
+//     console.log(i);
+// }
+//break task
+// for(var i=2; i<=10; i++){
+//     if(i==5 )
+//     break;
+//     console.log(i);
+// }
